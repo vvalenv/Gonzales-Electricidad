46e10c725b4e286aad932d3763473d567ad7d29f
val <valenvillar.neco@gmail.com>
2026-01-19 18:02:46 -0300

    nueva sintaxis

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/view', express.static(path.join(__dirname, 'dist/view')));
 app.use('/templates', express.static(path.join(__dirname, 'dist/view/html')));
-app.get('*', (req, res) => {
+app.get('(*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
